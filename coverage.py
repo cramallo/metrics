3c146948133f616f45cfc19ff54905dd0a24d567
cramallo <cramallo65@gmail.com>
2019-12-04 10:12:56 -0300

    typo fix

--- coverage.py
+++ coverage.py
@@ -4,10 +4,10 @@ import xml.etree.ElementTree as ET
 
 class Coverage:
     def __init__(self):
-        self.undercover_conditions = 0
+        self.uncovered_conditions = 0
         self.conditions_to_cover = 0
         self.lines_to_cover = 0
-        self.undercover_lines = 0
+        self.uncovered_lines = 0
         self.lc = 0
         self.total_lines_code = 0
         self.code_coverage = 0
@@ -26,18 +26,18 @@ class Coverage:
                     self.cyclomatic_complexity = int(
                         elem.attrib['missed']) + int(elem.attrib['covered'])
                 elif(elem.attrib['type'] == 'BRANCH'):
-                    self.undercover_conditions = int(elem.attrib['missed'])
+                    self.uncovered_conditions = int(elem.attrib['missed'])
                     self.conditions_to_cover = int(
-                        elem.attrib['covered']) + self.undercover_conditions
+                        elem.attrib['covered']) + self.uncovered_conditions
                 elif(elem.attrib['type'] == 'LINE'):
-                    self.undercover_lines = int(elem.attrib['missed'])
+                    self.uncovered_lines = int(elem.attrib['missed'])
                     self.lines_to_cover = int(
-                        elem.attrib['covered']) + self.undercover_lines
-                    self.lc = self.lines_to_cover - self.undercover_lines
+                        elem.attrib['covered']) + self.uncovered_lines
+                    self.lc = self.lines_to_cover - self.uncovered_lines
 
     def calculate_code_coverage(self):
         self.set_variables()
-        self.code_coverage = (self.conditions_to_cover - self.undercover_conditions + self.lc) \
+        self.code_coverage = (self.conditions_to_cover - self.uncovered_conditions + self.lc) \
             / (self.conditions_to_cover + self.lines_to_cover) * 100
 
         print('Code coverage ratio: ' + str(round(self.code_coverage, 2)) + "%")
